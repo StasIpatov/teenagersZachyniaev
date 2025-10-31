8486efa140969719bdae5bf614b07ff1d5d715ff
StasIpatov <166314021+StasIpatov@users.noreply.github.com>
2025-10-31 11:05:25 +0200

    Add files via upload

--- JS/search.js
+++ JS/search.js
@@ -4,13 +4,11 @@ search.addEventListener('change', function() {
   let searchV = search.value;
   console.log('Вибрано категорію:', searchV);
 
-  // Знайдемо всі картки
   let people = document.querySelectorAll('.person');
 
   people.forEach(person => {
     let roles = person.dataset.role.split(',');
 
-    // якщо "Усі" або співпадає з будь-якою роллю
     if (searchV == 0 || roles.includes(searchV)) {
       person.style.display = 'block';
     } else {
